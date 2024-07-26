cac4fed314cc7728d5eeb7324e3974943a16ed3a
AhmedHamadto <Ahmed.Abdelmuniem@gmail.com>
2024-07-26 11:05:31 +0800

    Initialised the repo

---
+++ Arrays/arrays.py
@@ -0,0 +1,22 @@
+import numpy as np
+# [Provide a description of numpy]
+
+# Create a 1D array
+a = np.array([1, 2, 3, 4, 5])
+# print(a)
+
+# Printing the first element of an array
+# print(a[4])
+
+# # Create a 2D array
+b = np.array([[1, 2, 3], [4, 5, 6]])
+print(b)
+
+# # Create a 3D array
+# c = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+# # Create a 4D array
+# d = np.array([[[[1, 2], [3, 4]], [[5, 6], [7, 8]]], [[[9, 10], [11, 12]], [[13, 14], [15, 16]]]])
+
+# # Create an array of all zeros
+# d = np.zeros((2, 3))
---
+++ Introduction to Python/practice.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter a number"))
+second_number = int(input("Enter a second number"))
+result = first_number + second_number
+print(result)
